5f386b7f44020ad4d8739e6e815b92bfd9cc9971
Alex Rothenberg <alex@alexrothenberg.com>
2020-10-27 16:50:48 -0400

    adding some ember concurrency goodness

--- app/components/classifier.js
+++ app/components/classifier.js
@@ -1,15 +1,28 @@
 import Component from '@glimmer/component';
 import { action} from '@ember/object';
 import { tracked} from '@glimmer/tracking';
-import fetch from 'fetch';
+import { dropTask } from 'ember-concurrency-decorators';
+import {load}  from "nsfwjs";
 
 export default class ClassifierComponent extends Component {
   @tracked
   predictions = [];
 
+  constructor() {
+    super(...arguments);
+
+    this.loadModel.perform();
+  }
+
+  @dropTask
+  *loadModel() {
+    return yield load('/tfjs_quant_nsfw_mobilenet/');
+  }
+
   @action
   async classify({target}) {
-    this.predictions = await this.args.model.classify(target);
+    const model = this.loadModel.lastSuccessful.value
+    this.predictions = await model.classify(target);
   }
 
 }
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,3 @@
 import Route from '@ember/routing/route';
-import * as nsfwjs from "nsfwjs";
 
-export default class ApplicationRoute extends Route {
-  model() {
-    return nsfwjs.load('/tfjs_quant_nsfw_mobilenet/');
-  }
-}
+export default class ApplicationRoute extends Route {}
